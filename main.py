f371cd227642c9132d3b516950e5e8efb0701c3f
Cezar-22 <111129815+Cezar-22@users.noreply.github.com>
2022-12-03 16:06:20 +0200

    added repeat loop

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 import random
+password = ""
 characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", "&", "?", "@", "*", "$", "%", "#"]
-n1 = characters[random.randint(1, 69)]
-n2 = characters[random.randint(1, 69)]
-n3 = characters[random.randint(1, 69)]
-n4 = characters[random.randint(1, 69)]
-n5 = characters[random.randint(1, 69)]
-n6 = characters[random.randint(1, 69)]
-n7 = characters[random.randint(1, 69)]
-n8 = characters[random.randint(1, 69)]
-n9 = characters[random.randint(1, 69)]
-n10 =characters[random.randint(1, 69)]
-print(n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + n10)
+for x in range(10):
+  nr = characters[random.randint(0,68)]
+  password += nr
+print(password)
