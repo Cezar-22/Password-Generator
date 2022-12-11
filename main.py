732110ffae0f6231f4a890862c8e7edd87f16a65
Cezar <cezar@bailescu.ro>
2022-12-11 19:35:25 +0200

    added password variable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+password = ""
 characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", "&", "?", "@", "*", "$", "%", "#"]
 n1 = characters[random.randint(1, 69)]
 n2 = characters[random.randint(1, 69)]
@@ -10,4 +11,5 @@ n7 = characters[random.randint(1, 69)]
 n8 = characters[random.randint(1, 69)]
 n9 = characters[random.randint(1, 69)]
 n10 =characters[random.randint(1, 69)]
-print(n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + n10)
+password = n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + n10
+print(password)
